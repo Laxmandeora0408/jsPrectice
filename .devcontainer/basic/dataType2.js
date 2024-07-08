5404d6cda3eab98e877b1aa3c5fae617520751db
Laxman Deora <93597180+Laxmandeora0408@users.noreply.github.com>
2024-07-08 18:26:32 +0000

    datatype2

---
+++ .devcontainer/basic/dataType2.js
@@ -0,0 +1,49 @@
+//let isLoggedIn = true;
+//let isLoggedOut =false;
+/*
+let numberValue = String(score)
+console.log(numberValue);//33
+console.log(typeof(numberValue));//String
+*/
+
+/*
+let numberValue = Boolean(score)
+console.log(numberValue);//1
+console.log(typeof(numberValue));//boolean
+*/
+
+/*
+let numberValue = Symbol(score)//Symbol(33)
+console.log(numberValue);
+console.log(typeof(numberValue))//Symboll
+
+*/
+
+/*
+let numberValue = Number(isLoggedIn)
+let numVal1 = Number(isLoggedOut);
+console.log(numberValue);//1
+console.log(numVal1);//0
+console.log(typeof(numberValue))//number
+console.log(typeof(numVal1));//number
+
+*/
+
+//let name = 'laxman'
+// let stringNumber = Number(name);
+ //console.log(stringNumber);//NaN
+
+
+/*
+let name = 'laxman'
+ let stringNumber = Boolean(name);
+ console.log(stringNumber);//NaN
+ console.log(typeof(stringNumber));//boolean
+ */
+
+let name;
+let nameV = String(name);
+console.log(nameV);
+console.log(typeOf(nameV)); // not defined
+
+
