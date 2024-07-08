9916fd58ab4dec6ede1ef297a03d1e8c3278a5f2
Laxman Deora <93597180+Laxmandeora0408@users.noreply.github.com>
2024-07-08 17:30:33 +0000

    basic

--- .devcontainer/basic/text.js
+++ .devcontainer/basic/text.js
@@ -1,1 +1,40 @@
-console.log("hell world");
+console.log(accountPassword);
+/* if you use var variable then its show undefine 
+
+
+/*console.log(accountEmail);
+if u use let keyword then
+cannot access before initialization 
+*/
+
+
+const accountId  = 1234;
+let accountEmail = "laxmandeora887@gmail.com";
+var accountPassword = "laxmandeora";
+accountCity = "Jaipur"
+accountState =""
+
+//console.log(accountId);//
+//console.log(accountEmail);//
+//console.log(accountPassword);//
+
+
+//accountId = 2 // not allowed because constant value not reasign
+
+/*
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
+/*
+console.log(typeof(accountCity));
+console.log(accountState); //undefine: not befine the value
+console.log(typeof(accountState));//its also u
+ndefine
+
+*/
+
+/*
+prefer not to use var
+because of issue in block scope and function scope
+ 
+
+*/
