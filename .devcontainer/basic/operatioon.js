b05efc7bd8a669ada558337a5ec4c5521b68abec
Laxman Deora <93597180+Laxmandeora0408@users.noreply.github.com>
2024-07-08 18:51:27 +0000

    new

--- .devcontainer/basic/operatioon.js
+++ .devcontainer/basic/operatioon.js
@@ -6,4 +6,19 @@ console.log(2**2);
 
 console.log(-true);//-1
 console.log(+''); 0
-console.log("hello");
+//console.log("hello"+); //not run
+
+console.log("2">1);//true
+console.log("02"<1);//false
+console.log(typeof '02'<1);// false
+console.log(1<"2"); //true
+
+
+console.log(null>0);//false
+console.log(null == 0);//false
+console.log(null<0)//false
+
+console.log(undefined>0);//false
+console.log(undefined == 0);//false
+console.log(undefined<0)//false
+
