b8a1dc82649b9dbe38edd3a5e917779595188f26
Laxman Deora <93597180+Laxmandeora0408@users.noreply.github.com>
2024-07-12 18:29:46 +0000

    async

---
+++ .devcontainer/02_interview/asyncSync.js
@@ -0,0 +1,15 @@
+function myFather() {
+  console.log("6-9AM papa dukan par rahenge");
+}
+
+function mySister() {
+  myFather();
+  console.log("9-4PM didi dukan par rhegi");
+}
+
+function myDuty() {
+  mySister();
+  console.log("4-8PM ye he meri duty he");
+}
+
+myDuty();
